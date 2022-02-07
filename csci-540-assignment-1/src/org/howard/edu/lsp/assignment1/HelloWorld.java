2c1df806851c3a43ca43684b72a748e5f1e86ef4
Ben Corriette <gentleben8282@yahoo.com>
2022-02-06 19:19:30 -0500

    These are the project files for Assignment #1.

---
+++ csci-540-assignment-1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,18 @@
+/* CSCI 540: Assignment #1
+ * Programmer: Ben Corriette
+ * Student ID: @02956064
+ * Last modified: 02/06/2022
+ * 
+ * Summary: Hello World program in Java programming language.
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// Output "Hello World" message to the console
+		System.out.println("Hello World!");
+	}
+
+}
